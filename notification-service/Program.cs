0285d7da9783f335812f619dc3b0a4161dfe9c04
safa-alaloosi <s.alaloosi4462@student.nu.edu>
2025-01-19 13:19:29 +0530

    initialized notification service

---
+++ notification-service/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/health", () => "healthy")
+    .WithName("Health")
+    .WithOpenApi();
+
+app.Run();
